b6c5eff2bb3d659b1af682219e018da751cf36b2
Chip <Panopticon772@gmail.com>
2018-08-06 22:54:26 -0400

    Form change complete.

--- main/tips.js
+++ main/tips.js
@@ -1,21 +1,14 @@
-// enter check amount
-
-// enter tip as %
-
-// when button clicked, takes check amount + tip % and calculates tip amount, and also provides a total amount
-
-// design
-
-// selectors/target button
+// selectors/target buttons
 const calcButton = document.querySelector("#calculate");
 const selectAmount = document.querySelector("#amount");
 const selectTip = document.querySelector("#tip-percent");
 
+// Math
 calcButton.addEventListener("click", () => {
     let amount = selectAmount.value;
     let tip = selectTip.value / 100;
     let totalTip = Math.round(amount * tip);
-    let total = +amount +totalTip;
+    let total = Math.round((+amount +totalTip) * 100) / 100;
     document.querySelector("#tip-amount").innerHTML = `Tip: $${totalTip}`;
     document.querySelector("#total").innerHTML = `Total: $${total}`;
 });
