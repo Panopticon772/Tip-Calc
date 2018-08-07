4c61721c2cfc377674ca6318de9db78e7c1db0a0
Chip <Panopticon772@gmail.com>
2018-08-07 18:12:37 -0400

    Added modal overlay.

--- main/tips.js
+++ main/tips.js
@@ -2,13 +2,28 @@
 const calcButton = document.querySelector("#calculate");
 const selectAmount = document.querySelector("#amount");
 const selectTip = document.querySelector("#tip-percent");
+const getModal = document.getElementById("my-modal");
+const getSpan = document.getElementsByClassName("close-modal")[0];
 
-// Math
+// math + open modal
 calcButton.addEventListener("click", () => {
     let amount = selectAmount.value;
     let tip = selectTip.value / 100;
     let totalTip = Math.round(amount * tip);
     let total = Math.round((+amount +totalTip) * 100) / 100;
+    getModal.style.display = "block";
     document.querySelector("#tip-amount").innerHTML = `Tip: $${totalTip}`;
     document.querySelector("#total").innerHTML = `Total: $${total}`;
 });
+
+// close modal x
+getSpan.addEventListener("click", () => {
+    getModal.style.display = "none";
+});
+
+// close modal by clicking anywhere outside
+window.onclick = (e) => {
+    if (e.target === getModal) {
+        getModal.style.display = "none";
+    }
+}
